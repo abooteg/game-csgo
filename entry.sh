8b7c2d72750c9153b5fc0e0dcbc4d1d7d7377ac2
booteg <ais@aiss-MacBook-Pro.local>
2021-12-09 14:16:49 +0300

    1

--- entry.sh
+++ entry.sh
@@ -6,27 +6,6 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
 				+app_update "${STEAMAPPID}" \
 				+quit
 
-# We assume that if the config is missing, that this is a fresh container
-if [ ! -f "${STEAMAPPDIR}/${STEAMAPP}/cfg/server.cfg" ]; then
-	# Download & extract the config
-	wget -qO- "${DLURL}/master/etc/cfg.tar.gz" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}"
-	
-	# Are we in a metamod container?
-	if [ ! -z "$METAMOD_VERSION" ]; then
-		LATESTMM=$(wget -qO- https://mms.alliedmods.net/mmsdrop/"${METAMOD_VERSION}"/mmsource-latest-linux)
-		wget -qO- https://mms.alliedmods.net/mmsdrop/"${METAMOD_VERSION}"/"${LATESTMM}" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}"	
-	fi
-
-	# Are we in a sourcemod container?
-	if [ ! -z "$SOURCEMOD_VERSION" ]; then
-		LATESTSM=$(wget -qO- https://sm.alliedmods.net/smdrop/"${SOURCEMOD_VERSION}"/sourcemod-latest-linux)
-		wget -qO- https://sm.alliedmods.net/smdrop/"${SOURCEMOD_VERSION}"/"${LATESTSM}" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}"
-	fi
-
-	# Change hostname on first launch (you can comment this out if it has done it's purpose)
-	sed -i -e 's/{{SERVER_HOSTNAME}}/'"${SRCDS_HOSTNAME}"'/g' "${STEAMAPPDIR}/${STEAMAPP}/cfg/server.cfg"
-fi
-
 # Check if autoexec file exists. Easier to copy config between servers. (I was migrating when I wrote this)
 # Passing them directly to srcds_run to ignores values set in autoexec.cfg.
 autoexec_file="${STEAMAPPDIR}/${STEAMAPP}/cfg/autoexec.cfg"
